d0f27da89712f5d052777be0fc809456d25bb50c
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-10 15:56:39 -0500

    Added --after option to CLI#list. Reverse of --before option.

--- lib/blitz_bulletins/cli.rb
+++ lib/blitz_bulletins/cli.rb
@@ -5,9 +5,11 @@ module BlitzBulletins
 
     desc "list", "List the Blitz Bulletin topics"
     method_option :before, :type => :string, :aliases => '-b'
+    method_option :after, :type => :string, :aliases => '-a'
     def list
       topics.each do |t|
         next if not_before?(t.date)
+        next if not_after?(t.date)
         puts t
       end
     end
@@ -22,6 +24,16 @@ module BlitzBulletins
     def not_before?(date)
       return false unless before
       before == date ? true : before < date
+    end
+
+    def after
+      return nil unless options[:after]
+      @after ||= Date.parse(options[:after])
+    end
+
+    def not_after?(date)
+      return false unless after
+      after == date ? false : after > date
     end
 
     def topics
