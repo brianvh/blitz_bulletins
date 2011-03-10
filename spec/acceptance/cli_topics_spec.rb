9a63b47d1326733b3a6a5132a989357094835c4f
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-10 12:16:00 -0500

    Modified cli_topics_spec to create a tmp data/topics.txt file.

--- spec/acceptance/cli_topics_spec.rb
+++ spec/acceptance/cli_topics_spec.rb
@@ -3,10 +3,10 @@ require 'acceptance/acceptance_helper'
 describe "Listing Blitz Bulletin topics" do
   context "GIVEN: a topics file with 2 topics" do
     before(:each) do
-      topics_file = %{
-        Jan  1 04:02 topic-one
+      topics_file = %{Jan  1 04:02 topic-one
         Jan  2 04:02 topic-two
       }.gsub(/^ {8}/, '')
+      write_file("data/topics.txt", topics_file)
     end
 
     context "WHEN: we run `bb-topics list`" do
