b487241a94e95afb95f01689f506bdd394c949ea
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-10 11:11:23 -0500

    Initial commit of CLI class.
     * Added a require of the CLI class file to the main library.
     * Also added top-level module methods for loading the list of topics.

--- lib/blitz_bulletins.rb
+++ lib/blitz_bulletins.rb
@@ -1,3 +1,16 @@
+require 'blitz_bulletins/cli'
+
 module BlitzBulletins
-  # Your code goes here...
+  @@topics = []
+
+  def self.load_topics
+    return false unless @@topics.empty?
+    @@topics = BlitzBulletins::Topics.load and return true
+  end
+
+  def self.topics
+    load_topics
+    @@topics
+  end
+
 end
