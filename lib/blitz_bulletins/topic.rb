a4c8462f7a4cfcbccefbd58ec01953c51750224f
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-11 15:26:07 -0500

    Modified Topic to support getting the description.
     * Added with_desc argument and a new description attribute.
     * Modified Topic#name to return the name with description, if with_desc is true
       and the topic's name has an entry in the BlitzBulletins.descriptions hash.
     * Added an rspec example to verify Topic#to_s, when description is present.

--- lib/blitz_bulletins/topic.rb
+++ lib/blitz_bulletins/topic.rb
@@ -3,15 +3,17 @@ require 'date'
 module BlitzBulletins
 
   class Topic
-    attr_reader :name, :date
+    attr_reader :date
 
-    def initialize(name=nil, date=Date.today)
+    def initialize(name, date=Date.today, with_desc = false)
       @name = name
       @date = date
+      @description = nil
+      @with_desc = with_desc
     end
 
     def to_s
-      "#{@name}: #{short_date}"
+      "#{name}: #{short_date}"
     end
 
     def before?(before)
@@ -22,17 +24,33 @@ module BlitzBulletins
       after.nil? ? true : date >= after
     end
 
-    def self.from_line(line)
+    def name
+      description.nil? ? @name : "#{description} (#{@name})"
+    end
+
+    def description
+      @description ||= get_description
+    end
+
+    def self.from_line(line, with_desc = false)
       pat = /^(...) +(\d+) +(\d\d:?\d\d) (.+)$/
       mon, day, yr_time, name = pat.match(line).to_a[1..-1]
       yr_time = Date.today.year if /:/ === yr_time
-      Topic.new(name, Date.parse("#{mon} #{day} #{yr_time}"))
+      Topic.new(name, Date.parse("#{mon} #{day} #{yr_time}"), with_desc)
     end
 
     private
 
     def short_date
       @date.strftime("%m/%d/%Y")
+    end
+
+    def description?
+      @with_desc
+    end
+
+    def get_description
+      description? ? BlitzBulletins.descriptions[@name] : nil
     end
   end
 
