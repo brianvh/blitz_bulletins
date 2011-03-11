e2fea196af812e17f53a1cb9d78d61e9d79b96dc
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-11 14:31:59 -0500

    Added main module methods for loading and returning descriptions.

--- lib/blitz_bulletins.rb
+++ lib/blitz_bulletins.rb
@@ -4,10 +4,20 @@ require 'blitz_bulletins/topic'
 
 module BlitzBulletins
   @@topics = []
+  @@descriptions = {}
 
   def self.load_topics
     return false unless @@topics.empty?
     @@topics = BlitzBulletins::Topics.load and return true
+  end
+
+  def self.load_descriptions
+    return false unless @@descriptions.empty?
+    @@descriptions = BlitzBulletins::Descriptions.load and return true
+  end
+
+  def self.descriptions
+    @@descriptions
   end
 
   def self.topics
