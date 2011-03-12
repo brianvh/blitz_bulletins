7a960ec846440201c5faebbbe6de7d2cee2f2c0c
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-11 19:39:26 -0500

    Created poster_spec with examples to verify the Poster class.
     * Added an additional Struct and the mock_profile method to PostersHelper, so
       we can skip calling out to the DND, during unit testing.

--- spec/support/posters_helper.rb
+++ spec/support/posters_helper.rb
@@ -1,8 +1,13 @@
 module PostersHelper
   Struct.new("Poster", :uid, :topic, :line)
+  Struct.new("Profile", :name, :expires)
 
   def poster_line(uid, topic)
     Struct::Poster.new(uid, topic, "#{uid}:dartmouth.bulletins.#{topic}")
+  end
+
+  def mock_profile(name, expires)
+    Struct::Profile.new(name, expires)
   end
 
 end
