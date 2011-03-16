854197ad8dd83bc39b1b01b09b601f60c059263e
Brian V. Hughes <brianvh@dartmouth.edu>
2011-03-16 15:07:25 -0400

    PostersHelper#mock_profile now handles email accessor.

--- spec/support/posters_helper.rb
+++ spec/support/posters_helper.rb
@@ -1,13 +1,13 @@
 module PostersHelper
   Struct.new("Poster", :uid, :topic, :line)
-  Struct.new("Profile", :name, :expires)
+  Struct.new("Profile", :name, :email, :expires)
 
   def poster_line(uid, topic)
     Struct::Poster.new(uid, topic, "#{uid}:dartmouth.bulletins.#{topic}")
   end
 
   def mock_profile(name, expires)
-    Struct::Profile.new(name, expires)
+    Struct::Profile.new(name, "#{name.gsub(' ', '.')}@dartmouth.edu", expires)
   end
 
 end
