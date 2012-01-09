8fd56d645a8969fb01e847f072777048c45fe003
Brian V. Hughes <brianvh@dartmouth.edu>
2012-01-09 14:20:22 -0500

    Modified Topic to accept hash of attributes in .new.
     * Removed .from_line custom constructor. Data file line parsing is handled by
       the Topics class.
     * Updated the RSpec examples to match the new class API.

--- lib/blitz_bulletins/topic.rb
+++ lib/blitz_bulletins/topic.rb
@@ -1,13 +1,12 @@
 module BlitzBulletins
 
   class Topic
-    attr_reader :name, :date
+    attr_reader :name, :date, :description
 
-    def initialize(name, date=Date.today, with_desc = false)
-      @name = name
-      @date = date
-      @description = nil
-      @with_desc = with_desc
+    def initialize(attribs)
+      @name = attribs[:name]
+      @date = attribs[:date] || Date.today
+      @description = attribs[:description]
     end
 
     def to_s
@@ -30,29 +29,8 @@ module BlitzBulletins
       description.nil? ? name : "#{description} (#{name})"
     end
 
-    def description
-      @description ||= get_description
-    end
-
-    def self.from_line(line, with_desc = false)
-      pat = /^(...) +(\d+) +(\d\d:?\d\d) (.+)$/
-      mon, day, yr_time, name = pat.match(line).to_a[1..-1]
-      yr_time = Date.today.year if /:/ === yr_time
-      Topic.new(name, Date.parse("#{mon} #{day} #{yr_time}"), with_desc)
-    end
-
-    private
-
     def short_date
       @date.strftime("%m/%d/%Y")
-    end
-
-    def description?
-      @with_desc
-    end
-
-    def get_description
-      description? ? BlitzBulletins.descriptions[name] : nil
     end
   end
 
