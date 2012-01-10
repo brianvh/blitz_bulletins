911786ef70ceb9e09113081e3b5dd59a97663f83
Brian V. Hughes <brianvh@dartmouth.edu>
2012-01-10 11:32:11 -0500

    Added support to Topic for dealing with subscribers.
     * Updated topic_spec examples to verify subscriber output.

--- lib/blitz_bulletins/topic.rb
+++ lib/blitz_bulletins/topic.rb
@@ -1,20 +1,21 @@
 module BlitzBulletins
 
   class Topic
-    attr_reader :name, :date, :description
+    attr_reader :name, :date, :description, :subscribers
 
     def initialize(attribs)
       @name = attribs[:name]
       @date = attribs[:date] || Date.today
       @description = attribs[:description]
+      @subscribers = attribs[:subscribers]
     end
 
     def to_s
-      "#{full_name}: #{short_date}"
+      "#{full_name}: #{short_date}#{subscriber_count}"
     end
 
     def to_csv
-      [description, name, short_date].compact
+      [description, name, short_date, subscribers].compact
     end
 
     def before?(before)
@@ -31,6 +32,11 @@ module BlitzBulletins
 
     def short_date
       @date.strftime("%m/%d/%Y")
+    end
+
+    def subscriber_count
+      return '' if subscribers.nil?
+      " -- #{subscribers} subscriber#{subscribers == 1 ? '' : 's'}"
     end
   end
 
